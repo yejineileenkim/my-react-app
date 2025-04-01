e88a9286fff1e873f92f6a73d9e2a5165e4c220f
yejineileenkim <e.y.kim@wustl.edu>
2025-04-01 16:28:30 -0500

    import app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+
+import App from './App';
 
 function App() {
   const [count, setCount] = useState(0)
