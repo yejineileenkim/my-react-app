65065530ea0a296d95c13bd4adaa8299b187fca8
yejineileenkim <e.y.kim@wustl.edu>
2025-04-01 16:34:47 -0500

    board

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,29 @@ import App from './App';
 
 function App() {
   const [count, setCount] = useState(0)
+}
 
-  return (
+export default function Board() {
+  //...
+}  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
       </div>
-      <h1>Vite + React</h1>
-      <div clasName="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
     </>
-  )
-}
+  );
 
-export default function Square() {
-  return <button className="square">X</button>;
-}
+
+
